fe4df180ff315a93e0e3a2adb78f694c72769b19
kakaeve <zkzkdlqm@gmail.com>
2023-01-19 23:51:45 +0900

    Add files via upload

---
+++ js/axiosConnection.js
@@ -0,0 +1,21 @@
+const axios = require("axios");
+
+async function axiosConnection(method, url, data) {
+  return await axios({
+    method: method,
+    url: `${END_POINT}${url}`,
+    data: data,
+  });
+}
+
+async function randomImage() {
+  return await axios({
+    method: "get",
+    url: "https://api.unsplash.com/photos/random",
+    params: {
+      client_id: UNSPLASH_ACCESSS_KEY,
+    },
+  });
+}
+
+export { axiosConnection, randomImage };
