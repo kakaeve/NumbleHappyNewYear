1a0c6e716f9ed73f3cb44eb6e9efc11621bf1ce7
kakaeve <zkzkdlqm@gmail.com>
2023-01-20 16:58:41 +0900

    으아아아

--- js/axiosConnection.js
+++ js/axiosConnection.js
@@ -2,6 +2,9 @@ const axios = require("axios");
 
 const END_POINT = process.env.END_POINT;
 const UNSPLASH_ACCESSS_KEY = process.env.UNSPLASH_ACCESSS_KEY;
+
+console.log("end: ", END_POINT);
+console.log("api:", UNSPLASH_ACCESSS_KEY);
 async function axiosConnection(method, url, data) {
   return await axios({
     method: method,
